96ce5acd9aec9075e8e18ab7141df686955bf0ff
David <65242270+ddhuynh@users.noreply.github.com>
2020-10-18 10:25:26 -0700

    SQL

--- Database/T2_real_estate.sql
+++ Database/T2_real_estate.sql
@@ -1,26 +1,15 @@
--- Creating tables for T2_real_estate
-CREATE TABLE core (
+CREATE TABLE factor(
+	regionname INT,
+	city VARCHAR,
+	Incr_Per DECIMAL,
+	date INT,
+	Incr_type VARCHAR,
+	Year INT,
 	month_date_yyyymm INT,
 	postal_code INT,
 	zip_name VARCHAR,
 	active_listing_count_yy DECIMAL,
 	median_days_on_market_yy DECIMAL,
 	new_listing_count_yy DECIMAL,
-	pending_ratio_yy DECIMAL,
-	Year INT
+	pending_ratio_yy DECIMAL
 );
-
--- Creating tables for T2_real_estate
-CREATE TABLE Value_Incr_Combined (
-	regionname INT,
-	city VARCHAR,
-	Incr_Per DECIMAL,
-	date INT,
-	Incr_type VARCHAR,
-	Year INT
-);
-
-SELECT *
-FROM Value_Incr_Combined
-FULL OUTER JOIN core
-ON Value_Incr_Combined.Year = core.Year AND Value_Incr_Combined.regionname = core.postal_code;
