3f783dba8e8975b16576af643fc5d38c8794fe4e
David <65242270+ddhuynh@users.noreply.github.com>
2020-10-20 21:40:12 -0700

    Database

---
+++ T2_real_estate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE factor(
+	regionname INT,
+	city VARCHAR,
+	Incr_Per DECIMAL,
+	date INT,
+	Incr_type VARCHAR,
+	Year INT,
+	month_date_yyyymm INT,
+	postal_code INT,
+	zip_name VARCHAR,
+	active_listing_count_yy DECIMAL,
+	median_days_on_market_yy DECIMAL,
+	new_listing_count_yy DECIMAL,
+	pending_ratio_yy DECIMAL
+);
